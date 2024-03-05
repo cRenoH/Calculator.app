e604fd436b8e9595380ad38ac5f7195267b829dc
cRenoH <renook14@students.amikom.ac.id>
2024-03-05 11:58:38 +0700

    Coba coba 1

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,10 @@ class Calculator
 {
 	static void Main(string[]args)
 	{
+		int a = 10;
+		int b = 6;
 		
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2} ", a, b, Penambahan(a, b));
 		
 	}
 	
