33401b78a4ac4ff5c0eac8065cf4507463a7bd09
cRenoH <renook14@students.amikom.ac.id>
2024-03-05 12:07:14 +0700

    Penambahan Fungsi

--- Calculator.cs
+++ Calculator.cs
@@ -7,7 +7,19 @@ class Calculator
 		
 		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2} ", a, b, Penambahan(a, b));
 		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}"), a,b,, Pengurangan(a,b));
+		
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.RedKey();
 	}
 	
+	static int Penambahan(int a, int b)
+	{
+		return a+b;
+		
+	}
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
+	}
 }
 
